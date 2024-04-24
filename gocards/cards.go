ece91ba6d454ec6ca346a3740a016f375cf7dac9
Balamurugan R <siva.bala.89@gmail.com>
2024-04-24 20:23:25 +0530

    Whitespace formatting

--- gocards/cards.go
+++ gocards/cards.go
@@ -1,77 +1,77 @@
 package gocards
 
 import (
-        "fmt"
+	"fmt"
 )
 
 type CardSuit rune
 
 const (
-        HEARTS   CardSuit = '♥'
-        CLUBS    CardSuit = '♣'
-        DIAMONDS CardSuit = '♦'
-        SPADES   CardSuit = '♠'
+	HEARTS   CardSuit = '♥'
+	CLUBS    CardSuit = '♣'
+	DIAMONDS CardSuit = '♦'
+	SPADES   CardSuit = '♠'
 )
 
 type CardNumber string
 
 const (
-        ACE   CardNumber = "1"
-        TWO   CardNumber = "2"
-        THREE CardNumber = "3"
-        FOUR  CardNumber = "4"
-        FIVE  CardNumber = "5"
-        SIX   CardNumber = "6"
-        SEVEN CardNumber = "7"
-        EIGHT CardNumber = "8"
-        NINE  CardNumber = "9"
-        TEN   CardNumber = "10"
-        JACK  CardNumber = "J"
-        QUEEN CardNumber = "Q"
-        KING  CardNumber = "K"
+	ACE   CardNumber = "1"
+	TWO   CardNumber = "2"
+	THREE CardNumber = "3"
+	FOUR  CardNumber = "4"
+	FIVE  CardNumber = "5"
+	SIX   CardNumber = "6"
+	SEVEN CardNumber = "7"
+	EIGHT CardNumber = "8"
+	NINE  CardNumber = "9"
+	TEN   CardNumber = "10"
+	JACK  CardNumber = "J"
+	QUEEN CardNumber = "Q"
+	KING  CardNumber = "K"
 )
 
 type Card struct {
-        Suit   CardSuit
-        Number CardNumber
-        Show   bool
+	Suit   CardSuit
+	Number CardNumber
+	Show   bool
 }
 
 func (c Card) String() string {
-        show_str := "-"
-        if c.Show {
-                show_str = "0"
-        }
-        return fmt.Sprintf("%c%s %s", c.Suit, c.Number, show_str)
+	show_str := "-"
+	if c.Show {
+		show_str = "0"
+	}
+	return fmt.Sprintf("%c%s %s", c.Suit, c.Number, show_str)
 }
 
 type Pile struct {
-        Cards []Card
+	Cards []Card
 }
 
 func (pile *Pile) AddCard(card Card, idx int) {
-        if idx == -1 {
-                // log.Printf("idx is %d", idx)
-                idx = len(pile.Cards)
-        }
-        if len(pile.Cards) == idx {
-                pile.Cards = append(pile.Cards, card)
-                // log.Print("idx is ", pile)
-        } else {
-                pile.Cards = append(pile.Cards[:idx+1], pile.Cards[idx:]...)
-                pile.Cards[idx] = card
-        }
+	if idx == -1 {
+		// log.Printf("idx is %d", idx)
+		idx = len(pile.Cards)
+	}
+	if len(pile.Cards) == idx {
+		pile.Cards = append(pile.Cards, card)
+		// log.Print("idx is ", pile)
+	} else {
+		pile.Cards = append(pile.Cards[:idx+1], pile.Cards[idx:]...)
+		pile.Cards[idx] = card
+	}
 
 }
 
 func (pile *Pile) RemoveCard(idx int) {
-        if idx < len(pile.Cards) {
-                pile.Cards = append(pile.Cards[:idx], pile.Cards[idx+1:]...)
-        }
+	if idx < len(pile.Cards) {
+		pile.Cards = append(pile.Cards[:idx], pile.Cards[idx+1:]...)
+	}
 }
 
 func (pile *Pile) ShowCards(idxs []int) {
-        for _, idx := range idxs {
-                pile.Cards[idx].Show = true
-        }
+	for _, idx := range idxs {
+		pile.Cards[idx].Show = true
+	}
 }
